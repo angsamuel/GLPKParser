14395dc3ecffb4dff34d91387e02da878d626b8c
astralsquid <Brewmaster@brewmaster.trinity.local>
2016-10-23 15:18:49 -0500

    Fixed File Format

--- pGame/main.cpp
+++ pGame/main.cpp
@@ -5,8 +5,11 @@
 //  Created by Samuel Ang on 8/8/16.
 //  Copyright (c) 2016 Samuel Ang. All rights reserved.
 //
-
+#include <stdlib.h>
+#include <stdlib.h>
 #include <iostream>
+#include <fstream>
+#include <fstream>
 #include "board.h"
 #include <utility>
 #include <vector>
@@ -15,6 +18,8 @@
 using std::pair;
 using std::make_pair;
 using std::cout;
+using std::ifstream;
+using std::getline;
 int main(int argc, const char * argv[]) {
     //Board(int width, int height, vector<pair<int,int>> targetLocs, vector<double> targetProbs, pair<int, int> trueTargetLoc, pair<int, int> attackerStart, double mt, double gt, double ttr)
     int w = 3;
@@ -35,11 +40,100 @@ int main(int argc, const char * argv[]) {
     double ttr = 3;
     
     //[.5T]---[Start]---[.5F]
-    Board m(w, h, tL, tP, ttl, aS, mt, gt, ttr);
+    //Board m(w, h, tL, tP, ttl, aS, mt, gt, ttr);
+    //m.printAllNodeInfo();
+    
+    //LpGenerator a(m);
+    //a.convertToLp();
+    
+    //string inputFile = "lpFile.in";
+    int width;
+    int height;
+    vector<pair<int, int>> targetLocations;
+    pair<int,int> trueTargetLocation;
+    vector<double> probDistribution;
+    pair<int, int> attackerStart;
+    int moveCost;
+    int predictionReward;
+    int trueTargetReward;
+    ifstream inFile;
+    inFile.open("lpFile.in");
+    string line;
+    while(inFile >> line){
+        if(line == "width"){
+            //cout << line << "|\n";
+            inFile >> line;
+            //cout << line << "||\n";
+            width = atoi(line.c_str());
+            //cout << width << "|||\n";
+        }
+        if(line == "height"){
+            inFile >> line;
+            height = atoi(line.c_str());
+            
+        }
+        //cout << "line: " <<line << "\n";
+        if(line == "target_locations"){
+            inFile >> line;
+            while(line!= "probability_distribution"){
+                int tlX = atoi(line.substr(0,1).c_str());
+                int tlY = atoi(line.substr(2,1).c_str());
+                //cout << "tlX " << tlX << "\n";
+                targetLocations.push_back(make_pair(tlX,tlY));
+                inFile >> line;
+            }
+            
+        }
+        if(line == "probability_distribution"){
+            inFile >> line;
+            while(line!= "true_target_location"){
+                double prob = atof(line.c_str());
+                //cout << "prob: " << prob << "\n";
+                probDistribution.push_back(prob);
+                inFile >> line;
+            }
+        }
+        if(line == "true_target_location"){
+            inFile >> line;
+            int tlX = atoi(line.substr(0,1).c_str());
+            int tlY = atoi(line.substr(2,1).c_str());
+            //cout << tlX << "tlx\n";
+            //cout << tlY << "tly\n";
+            trueTargetLocation = make_pair(tlX,tlY);
+        }
+        if(line == "attacker_start"){
+            inFile >> line;
+            int attackerStartX = atoi(line.substr(0,1).c_str());
+            int attackerStartY = atoi(line.substr(2,1).c_str());
+            attackerStart = make_pair(attackerStartX, attackerStartY);
+        }
+        if(line == "move_cost"){
+            inFile >> line;
+            moveCost = atoi(line.c_str());
+        }
+        if(line =="prediction_reward"){
+            inFile >> line;
+            predictionReward = atoi(line.c_str());
+        }
+        if(line =="true_target_reward"){
+            inFile >> line;
+            trueTargetReward = atoi(line.c_str());
+        }
+    }
+    
+    //cout << width << "\n";
+    //cout << w << "\n";
+    
+    //for(auto x : targetLocations){
+      //  cout << x.first << " * " <<  x.second << "\n";
+    //}
+    
+    Board m(width, height, targetLocations, probDistribution, trueTargetLocation, attackerStart, moveCost, predictionReward, trueTargetReward);
+    //Board m(width, height, targetLocations, probDistribution, trueTargetLocation, attackerStart, moveCost, predictionReward, trueTargetReward);
     m.printAllNodeInfo();
     
     LpGenerator a(m);
     a.convertToLp();
-    
+    //cout << width;
     return 0;
 }
